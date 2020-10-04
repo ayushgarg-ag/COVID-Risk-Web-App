b492deecda212a15937dddd6b9852f6d21a42a35
Donghan Park <donghan.park@duke.edu>
2020-10-03 22:36:29 -0400

    added react router

---
+++ covid-frontend/src/components/BasicClassroom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function BasicClassroom(){
+    return(
+        <div>
+            <h1>Basic Classroom Calculation Page</h1>
+        </div>
+    )
+}
+
+export default BasicClassroom;
