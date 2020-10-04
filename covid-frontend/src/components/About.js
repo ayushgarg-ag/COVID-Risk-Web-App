3e85a589288cf5ebea3d8b16b93f5a4d05d8a891
Donghan Park <donghan.park@duke.edu>
2020-10-03 23:10:21 -0400

    added about and home pages

--- covid-frontend/src/components/About.js
+++ covid-frontend/src/components/About.js
@@ -1,11 +1,50 @@
 import React from 'react';
 import '../App.css';
+import './About.css';
 
 function About(){
     return(
-        <div>
-            <h1>About Page</h1>
-        </div>
+        <nav className="About">
+            <div>
+                <h1>ABOUT THE MODEL</h1>
+                <p>
+                    This is a tool to help understand COVID-19 transmission risk to students and teachers/professors 
+                    due to transmission by microscopic airborne particles (i.e. aerosols) in classroom settings. 
+                    This is not an infectious disease dynamics model, but rather a model that predicts airborne virion 
+                    concentrations within a classroom, taking into account exhalation of virion-containing aerosols 
+                    by infected individuals and the loss of these particles due to various processes. Probabilities of 
+                    infection are calculated based on the virion dose inhaled (accounting for use of masks) by 
+                    uninfected people in the classroom.
+                    <br/><br/>
+                    The risk calculations here are only for disease transmission by the airborne aerosol route, and do 
+                    not account for transmission by droplets or from contaminated surfaces. The implicit assumption is 
+                    that appropriate social distancing and hygiene protocols are strictly adhered to in the classroom. 
+                    To the extent that this is not true, the risk of infection will be higher than predicted by these 
+                    calculations. Users should also also bear in mind that the absolute estimates of predicted risk from 
+                    this model are quite uncertain because of uncertainties in our knowledge of key parameters such as 
+                    the exhalation rate of virion-containing aerosols by infected individuals and the percentage of 
+                    infected individuals in the classroom. The model is nevertheless useful to explore the relative 
+                    effects of control measures (e.g. more ventilation, fewer people, shorter duration, masks vs no 
+                    masks) on COVID-19 transmission by aerosols in classrooms.
+                </p>
+            </div>
+            <div>
+                <h3>THE TEAM</h3>
+                <p>
+                    This probabilistic Monte Carlo framework was developed by <strong>Prasad Kasibhatla</strong>, as 
+                    an offshoot of the COVID-19 risk estimator developed by Jose Jimenez.
+                    <br/>
+                    <h3>FRONT END</h3>
+                    <p>List of names...</p>
+                    <h3>BACK END</h3>
+                    <p>List of names...</p>
+                    <h3>MODELING</h3>
+                    <p>List of names...</p>
+                    <h3>MOBILE DEVELOPMENT</h3>
+                    <p>List of names...</p>
+                </p>
+            </div>
+        </nav>
     )
 }
 
--- covid-frontend/src/components/Home.js
+++ covid-frontend/src/components/Home.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import '../App.css';
+import './Home.css';
 
 function Home(){
     return(
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <nav className="Home">
+            <h1>COVID-19<br/>EXPOSURE MODEL</h1>
+            <p>
+                This is a tool to help understand COVID-19 transmission risk to students 
+                and teachers/professors due to transmission by microscopic airborne particles 
+                in classroom settings.
+            </p>
+            <button>CALCULATE</button> <br/>
+            <button>HOW IT WORKS</button>
+        </nav>
     )
 }
 
