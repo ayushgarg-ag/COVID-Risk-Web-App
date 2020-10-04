a016798c2394254953de16854691bef26d366f7d
Helen Xiao <helen.xiao@duke.edu>
2020-10-04 01:24:07 -0400

    home page

--- covid-frontend/src/App.js
+++ covid-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
+import Home from './components/Home';
 import BasicClassroom from './components/BasicClassroom';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
@@ -11,8 +12,9 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/" exact component={About} />
-          <Route path="/basic_classroom" component={BasicClassroom} /> 
+          <Route path="/" exact component={Home} />
+          <Route path="/basic-classroom" component={BasicClassroom} /> 
+          <Route path="/about-us" component={About} /> 
         </Switch>
       </Router>
     </div>
