54b21b4055f7d747d865ddc8aab76b92e2cc716a
Kevin Yin <kevin.yin@duke.edu>
2020-10-04 00:37:25 -0700

    playing with routing

--- covid-frontend/src/App.js
+++ covid-frontend/src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import Header from './components/Header'
+import BasicLocation from './components/BasicLocation'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Router>
+       <div>
+           <ul>
+             <li>
+               <Link to="/basic">Basic Classroom</Link>
+             </li>
+           </ul>
+         <Switch>
+           <Route path="/basic" component={BasicLocation}>
+           </Route>
+         </Switch>
+       </div>
+     </Router>
     </div>
   );
 }
+
 
 export default App;
---
+++ covid-frontend/src/components/BasicClassroom.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BasicClassroom() {
+  return (
+    <>
+    <h1>BasicClassroom</h1>
+    </>
+  );
+}
+
+export default BasicClassroom;
