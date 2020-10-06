7c0ae63cf52e9a1c87f1476ad0e2a21dc6098a94
Ayush Garg <ayush.garg@duke.edu>
2020-10-05 22:45:39 -0400

    Added flask integration

---
+++ app.py
@@ -0,0 +1,15 @@
+import time
+import json
+from flask import Flask, request, render_template, jsonify
+app = Flask(__name__)
+
+@app.route('/formdata', methods=['POST', 'GET'])
+def formdata():
+    if request.method == 'POST':
+        content = request.data.decode()
+        content = json.loads(content)
+        print(content)
+        return {'result': "received"}
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000, debug=True)
