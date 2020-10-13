6e1b87fb780438f07e4c4447ad146b13cc6a719f
Helen Xiao <helen.xiao@duke.edu>
2020-10-12 20:56:22 -0400

    results page template + hover button

--- src/components/CalculateHome.js
+++ src/components/CalculateHome.js
@@ -2,23 +2,15 @@ import React from 'react';
 import './CalculateHome.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BsFillQuestionCircleFill } from "react-icons/bs";
-import Button from "react-bootstrap/Button";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
-import Popover from "react-bootstrap/Popover";
 
-function renderTooltip(props) {
+function renderBasicTooltip(props) {
+    return <Tooltip {...props}>The basic assessment is best for...</Tooltip>;
+}
+function renderAdvancedTooltip(props) {
     return <Tooltip {...props}>The advanced assessment is best for...</Tooltip>;
-  }
-  
-  const popover = (
-      <Popover>
-        <Popover.Title as="h1">Basic Test</Popover.Title>
-        <Popover.Content>
-          The basic assessment is best for...
-        </Popover.Content>
-      </Popover>
-    );  
+}
 
 function CalculateHome(){
 
@@ -28,13 +20,13 @@ function CalculateHome(){
             <a href="/locations-basic">
                 <button className="btn btn-primary">BASIC</button>
             </a>
-            <OverlayTrigger trigger="click" placement="right" overlay={popover}>
-            <Button size="sm" variant="success">What's this?</Button>
+            <OverlayTrigger placement="right" delay={{ show: 250, hide: 400 }} overlay={renderBasicTooltip}>
+                <BsFillQuestionCircleFill variant="success" />
             </OverlayTrigger><br/>
             <a href="/locations-advanced">
                 <button className="btn btn-primary">ADVANCED</button>
             </a>
-            <OverlayTrigger placement="right" delay={{ show: 250, hide: 400 }} overlay={renderTooltip}>
+            <OverlayTrigger placement="right" delay={{ show: 250, hide: 400 }} overlay={renderAdvancedTooltip}>
                 <BsFillQuestionCircleFill variant="success" />
             </OverlayTrigger>
         </nav> 
