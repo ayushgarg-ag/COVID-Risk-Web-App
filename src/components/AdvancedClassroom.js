f57df20697a4940afb03ef18c91d2c4b2f0dcc07
Helen Xiao <helen.xiao@duke.edu>
2020-10-28 16:21:28 -0400

    added pages for basic/adv for other settings

---
+++ src/components/AdvancedClassroom.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function AdvancedClassroom(){
+    return(
+        <nav className="centered-layout">
+            <h1 className="title">Advanced Classroom Calculation</h1>
+            <p>
+                Advanced Classroom Caluculation Coming Soon!
+            </p>
+            <a href="/calculate-home">
+                <button className="btn btn-primary">Go Back</button><br/>
+            </a>
+        </nav>
+    )
+}
+
+export default AdvancedClassroom;
---
+++ src/components/BasicCommonroom.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function BasicCommonroom(){
+    return(
+        <nav className="centered-layout">
+            <h1 className="title">Basic Commonroom Calculation</h1>
+            <p>
+                Basic Commonroom Caluculation Coming Soon!
+            </p>
+            <a href="/calculate-home">
+                <button className="btn btn-primary">Go Back</button><br/>
+            </a>
+        </nav>
+    )
+}
+
+export default BasicCommonroom;
