7911d00ac237b4aba86f2b4f59594dd28123e69c
Donghan Park <donghan.park@duke.edu>
2020-10-28 21:50:58 -0400

    added instructions

--- src/components/CustomSlider.js
+++ src/components/CustomSlider.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './CustomSlider.css';
 
+/*
+    To use slider as a custom HTML tag in any other component:
+
+    ex:     import CustomSlider from './CustomSlider';
+            <CustomSlider min='0' max='100' step_count='0.5'/>
+
+    min = minimum allowable value
+    max = maximum allowable value
+    step_count = step size of the sliders (interval between each increment)
+
+    To change styling:
+    -you can change any colors to whatever
+    -you can change 'width' and 'max-width' in lines 3-4 to change the width of the slider to whatever (make sure width is a percentage)
+    -plz try not to change anything else unless u understand what its doing cause a lot of scaling is hard-coded
+     and it was frickin annoying to set the correct values thnx... :)
+*/
+
 class CustomSlider extends React.Component{
     constructor(props){
         super(props);
 
         this.state = {
-            lvalue: parseInt(this.props.min), //this value holds the MIN input value
+            lvalue: parseInt(this.props.min), //this value holds the MIN input value (to be used for back-end)
             rvalue: parseInt(this.props.max), //this value holds the MAX input value
             _step: parseFloat(this.props.step_count)
         }
