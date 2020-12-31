419857c4dacb690b8e619c42d4d9711f04f1f0bb
Ayush Garg <ayush.garg@duke.edu>
2020-12-30 21:10:15 -0800

    changed gitignore

---
+++ client/build/precache-manifest.1e0a7dc9a12a4895f5d1d3d53a1aa39a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0495f6d2d2ebe38115c1e974794a9df3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da3bb9fdd924c5d39b7e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ebbc7be3ce4b6aef05b0",
+    "url": "/static/css/main.0d2b2c32.chunk.css"
+  },
+  {
+    "revision": "da3bb9fdd924c5d39b7e",
+    "url": "/static/js/2.14150c72.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.14150c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebbc7be3ce4b6aef05b0",
+    "url": "/static/js/main.35694cc5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
