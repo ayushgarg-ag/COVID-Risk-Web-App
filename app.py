539cca64ca413d15dc1bd88e32394eb2d09862ec
Ayush Garg <ayush.garg@duke.edu>
2020-12-30 18:53:15 -0800

    adjusted app

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ studentResults = None
 facultyResults = None
 
 
-# @app.errorhandler(404)
-# def not_found(e):
-#     return app.send_static_file('index.html')
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
 
 @app.route('/')
 def index():
